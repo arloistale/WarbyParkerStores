614e8036d58b14cc53e3fc7488967b2b007b73e2
arloistale <jonathanlu321@gmail.com>
2020-10-08 19:10:42 -0400

    Initial Commit

---
+++ WarbyParkerStores/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WarbyParkerStores
+//
+//  Created by Jonathan Lu on 10/8/20.
+//  Copyright © 2020 Jon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
